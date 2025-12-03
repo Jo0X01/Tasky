a8f3a378b8772745de69ec210167c424ae4797a6
Jo0X01 <joogithubme123@gmail.com>
2025-12-03 08:15:17 +0200

    feat(Splash Screen/OnBoarding Screen) - Created Splash Screen / OnBoarding Screen - Change App Luancher Icon - Created Some Constant Variables/Routes for future work global app work

---
+++ lib/features/splash/view/splash_screen.dart
@@ -0,0 +1,79 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasky/core/constant/app_constants.dart';
+import 'package:tasky/core/models/firebase/firebase_result.dart';
+import 'package:tasky/features/splash/data/firebase/splash_firebase_database.dart';
+import 'package:tasky/features/splash/data/models/onboarding_model.dart';
+import 'package:tasky/features/splash/view/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static const String routeName = AppRoutes.splashScreen;
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        if (!await navigateIfLoggedIn()) {
+          getDataThenNavigate();
+        }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff5F33E1),
+      body: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeInLeft(
+              duration: Duration(milliseconds: 900),
+              animate: true,
+              child: Image.asset(AssetConstant.taskIcon),
+            ),
+            BounceInDown(
+              from: 50,
+              animate: true,
+              delay: Duration(milliseconds: 900),
+              duration: Duration(milliseconds: 600),
+              child: Image.asset(AssetConstant.yIcon),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<bool> navigateIfLoggedIn() async {
+    final isLoggedInBefore = FirebaseAuth.instance.currentUser != null;
+    if (isLoggedInBefore) {
+      await Future.delayed(Duration(seconds: 2));
+      Navigator.of(context).pushReplacementNamed(AppRoutes.homeScreen);
+    }
+    return isLoggedInBefore;
+  }
+
+  void getDataThenNavigate() async {
+    final onboardingData = await SplashFirebaseDatabase.getOnboardingData();
+    await Future.delayed(Duration(seconds: 2));
+    switch (onboardingData) {
+      case FBResultSuccess<List<OnboardingModel>>():
+        Navigator.of(context).pushReplacementNamed(
+          OnboardingScreen.routeName,
+          arguments: onboardingData.data,
+        );
+      case FBResultError<List<OnboardingModel>>():
+        Navigator.of(context).pushReplacementNamed(AppRoutes.loginScreen);
+    }
+  }
+}
